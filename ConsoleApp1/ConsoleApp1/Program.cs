b6b80a0b3c56ae006b2cfcccbb4c62732e25ddff
MikeGolf1998 <S01173793@acad.tri-c.edu>
2018-10-29 22:27:22 -0400

    Lab 1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Commenting Hello from Mike Damiano
 namespace HelloWorld
 {
     class Hello
